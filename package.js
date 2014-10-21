ca77b919817e6c9d4beb998801adb78ea164f686
garrilla <garry@garrilla.com>
2014-10-21 22:32:02 +0100

    first test

---
+++ package.js
@@ -0,0 +1,20 @@
+Package.describe({
+  name: 'garrilla:3r',
+  summary: 'A small package to make Meteor  apps respond to device rotation reactively, no CSS.',
+  version: '0.0.1',
+  git: 'https://github.com/garrilla/garrilla-3r.git'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('0.9.4');
+  api.addFiles('body.html','client');
+  api.addFiles('garrilla:3r.html','client');
+  api.addFiles('garrilla:3r.js','client');
+  api.use(['templating'],'client')
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('garrilla:3r');
+  api.addFiles('garrilla:3r-tests.js');
+});
